9d52bc9e99b334ac323b0f4c6c0c1a03bc96ac72
Peter Murphy <petersmurphy7@gmail.com>
2023-04-26 20:59:39 -0400

    override auth scopes in my authOptions

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,25 +1,28 @@
 import NextAuth from 'next-auth'
 import Spotify from 'next-auth/providers/spotify'
 
+// TODO: probably narrow down these scopes
+const scopes = [
+  'playlist-read-private',
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-top-read',
+]
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
     Spotify({
       clientId: process.env.SPOTIFY_CLIENT_ID,
       clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      authorization: `https://accounts.spotify.com/authorize?scope=${scopes.join(
+        '%20'
+      )}`,
     }),
   ],
   theme: {
     colorScheme: 'dark',
   },
   secret: process.env.SPOTIFY_CLIENT_SECRET,
-  // callbacks: {
-  //   session: async (session, user) => {
-  //     if (session?.user) {
-  //       session.user.id = user.id
-  //     }
-  //     return session
-  //   },
-  // },
 }
 export default NextAuth(authOptions)
